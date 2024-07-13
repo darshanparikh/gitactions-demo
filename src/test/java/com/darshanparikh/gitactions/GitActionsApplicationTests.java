f4df663dd7683fb7d41d85a93869e57008de0a60
Darshan Parikh <darshan1191@gmail.com>
2024-07-13 00:54:24 -0400

    Initial commit

---
+++ src/test/java/com/darshanparikh/gitactions/GitActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darshanparikh.gitactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
